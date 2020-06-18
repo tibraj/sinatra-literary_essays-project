00d7cef4e2db8781327ad52a23aee18a5f005c08
tibraj <tibraj94@gmail.com>
2020-06-18 12:05:43 -0400

    created routes for creating and accessiing specific literary essays

--- app/controllers/literary_essays_controller.rb
+++ app/controllers/literary_essays_controller.rb
@@ -1,7 +1,24 @@
 class LiteraryEssaysController < ApplicationController
 
     get "/literary_essays/new" do 
-      erb :
+      erb :'/literary_essays/new'
+    end 
+
+    post '/literary_essays' do 
+        if !logged_in? 
+            redirect '/'
+        end 
+        if params[:title] != "" && params[:content] != ""
+            @literary_essay = LiteraryEssay.create(title: params[:title], content: params[:content], user_id: current_user.id)
+            redirect "/literary_essays/#{@literary_essay.id}"
+        else 
+            redirect '/literary_essays/new'
+        end
+    end 
+
+    get '/literary_essays/:id' do 
+        @literary_essay = LiteraryEssay.find(params[:id])
+        erb :'/literary_essays/show'
     end 
 
 end 
