221b7724cb93bac7b5efe6a98470961314ae5e90
tibraj <tibraj94@gmail.com>
2020-06-18 14:30:00 -0400

    added edite routes, conditional logic  for user protection

--- app/controllers/literary_essays_controller.rb
+++ app/controllers/literary_essays_controller.rb
@@ -17,8 +17,40 @@ class LiteraryEssaysController < ApplicationController
     end 
 
     get '/literary_essays/:id' do 
-        @literary_essay = LiteraryEssay.find(params[:id])
+        set_literary_essay
         erb :'/literary_essays/show'
     end 
 
+    get '/literary_essays/:id/edit' do
+        set_literary_essay
+        if logged_in?
+            if @literary_essay.user == current_user
+                erb :'/literary_essays/edit'
+            else 
+                redirect "/users/#{current_user.id}"
+            end 
+        else 
+            redirect'/'
+        end
+    end 
+
+    patch '/literary_essays/:id' do 
+        set_literary_essay
+        if logged_in?
+            if @literary_essay.user == current_user
+                @literary_essay.update({title: params[:title], content: params[:content]})
+                redirect "/literary_essays/#{@literary_essay.id}"
+            else 
+                redirect "users/#{current_user.id}"
+            end 
+        else 
+            redirect '/'
+        end
+    end 
+
+    private
+    
+    def set_literary_essay
+        @literary_essay = LiteraryEssay.find(params[:id])
+    end 
 end 
